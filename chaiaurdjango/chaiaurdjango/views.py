76eddf34803645820dcdabf07c3d2bed533b0094
Aayush-002 <dahalaayush968@gmail.com>
2024-06-11 11:12:02 +0545

    Initial commit

---
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("You are at chai aur django homepage.")
+
+def home(request):
+    return HttpResponse("You are at chai aur django homepage.")
