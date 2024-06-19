729db5d9e563cf0048ac3e38d1c108361eb0338d
Aayush-002 <dahalaayush968@gmail.com>
2024-06-19 16:48:36 +0545

    Next Commit

--- chaiaurdjango/chaiaurdjango/views.py
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("You are at chai aur django homepage.")
+    return render(request,"website/index.html")
+    # return HttpResponse("You are at chai aur django homepage.")
 
-def home(request):
-    return HttpResponse("You are at chai aur django homepage.")
+def about(request):
+    return HttpResponse("You are at chai aur django about.")
+
+def contact(request):
+    return HttpResponse("You are at chai aur django contact.")
